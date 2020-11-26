40118372fded9fc3912efb65d592a7b53122e937
akshayauti <akshayauti3260@gmail.com>
2020-11-26 15:44:27 +0530

    Dashboard changes

--- client/src/app/fx-dashboard/fx-dashboard.component.ts
+++ client/src/app/fx-dashboard/fx-dashboard.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵALLOW_MULTIPLE_PLATFORMS } from '@angular/core';
 
 @Component({
   selector: 'app-fx-dashboard',
   templateUrl: './fx-dashboard.component.html',
-  styleUrls: ['./fx-dashboard.component.css']
+  styleUrls: ['./fx-dashboard.component.css' ]
 })
 export class FxDashboardComponent implements OnInit {
 
